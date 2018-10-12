d213f2eb08312f4094b6573e4fff541d6ad56477
Nitin Chhabra <nchhabra@yodlee.com>
2018-10-12 15:38:26 +0530

    Fisrt Commit

---
+++ main/java/com/yodlee/ConnectionUtility.java
@@ -0,0 +1,32 @@
+package main.java.com.yodlee;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ConnectionUtility {
+
+	public HttpURLConnection createConnection(String jenurl) throws IOException
+	{
+		
+		URL url = new URL(jenurl);
+		HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+		connection.setRequestMethod("GET");
+		 String userpass = "builder" + ":" + "CIAdmin4dev";
+		 String basicAuth = "Basic " + javax.xml.bind.DatatypeConverter.printBase64Binary(userpass.getBytes());
+		 connection.setRequestProperty("Accept", "application/xml");
+		 connection.setRequestProperty("Content-Type", "application/xml");
+
+
+		 connection.setRequestProperty ("Authorization", basicAuth);
+		connection.connect();
+		return connection;
+	}
+
+	public void closeConnection(HttpURLConnection connection)
+	{
+		connection.disconnect();
+			
+	}
+	
+}
---
+++ main/java/com/yodlee/Main.java
@@ -0,0 +1,27 @@
+package main.java.com.yodlee;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.json.JSONException;
+import org.xml.sax.SAXException;
+
+public class Main {
+
+	public static void main(String[] args) throws JSONException, IOException, ParserConfigurationException, SAXException {
+
+ListJobs lj = new ListJobs();
+String output=lj.fetchJoblist("http://192.168.113.195:9090/api/json?pretty=true");
+Map<String, String> jobMap= lj.parseJsondata(output);
+
+jobXmlParser jxp = new jobXmlParser();
+
+jxp.jbXmlParser(jobMap);
+JobJsonParsing jjp = new JobJsonParsing();
+jjp.fetchJobData(jobMap);
+
+	}
+
+}
